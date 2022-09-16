3def147a77024f7f964ec41735606a45a894cde6
Julio Renteria <jarenteriaruiz@gmail.com>
2022-09-16 16:49:25 -0500

    Cambie el nombre de la pagina de nosotros en el header

--- nosotros.php
+++ nosotros.php
@@ -2,7 +2,7 @@
 
 
 <div class="jumbotron">
-    <h1 class="display-3">Nosotros</h1>
+    <h1 class="display-3">Nosotros+</h1>
     <p class="lead">Hola somos uan empresa de tecnologis y queremos prestarte el mejor servicio </p>
     <hr class="my-2">
 
